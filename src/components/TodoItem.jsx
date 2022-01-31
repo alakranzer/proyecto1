1261dc3fbb72e0ff0f697795a450d7cfa463e549
paulbeltran <alex.rocha.s@gmail.com>
2022-01-31 12:27:55 -0300

    Se actualizó separación de botones checkbox y textbox

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -5,16 +5,19 @@ export function TodoItem({todo}){
     const {id, titulo, descripcion, importancia} = todo;
 
 
-    return <div className="list-group-item">
-                <div className="row row-cols" style={{width:'20rem' }}>
-                     <div className="card-body">
-                         <div className="card-header">{titulo}<button className="btn btn-black ms-5">X</button>
-                             <div className="card-text">
-                                    <p className="card-text">{descripcion}</p>
-                            </div>
+    return <div class="row" col-3>
+                 <div className="card">
+                    <div className="card-body">
+                     <div className="card-header">{titulo}<button className="btn btn-black ms-5">X</button>
+                        <div className="card-text">
+                            <p className="card-text">{descripcion}</p>
                         </div>
                     </div>
                 </div>
             </div>
+         </div>
+       
         
 }
+
+
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,3 +1,4 @@
+
 import React, { Fragment, useState, useRef, useEffect } from 'react'
 import { v4 as uuid } from 'uuid';
 import { TodoItem } from './TodoItem';
@@ -102,19 +103,31 @@ export function TodoList(){
             <h1>Post it Simulator</h1>
 
             <div className="input-group mb-3">
+            <form class="row g-3">
+            <div class="col-auto">
                 <input ref={titRef} placeholder='Título' className="form-control" type="text"></input>
-                <input ref={descripRef} placeholder='Descripción' className="form-control" type="text"></input>
-                <input ref={impoRef} type="checkbox" className="form-check-input me-2" id="importancia"></input>Importante
-                
-       
-                <button onClick={agregarTarea} className="btn btn-info ms-1">Ingresar Post it<i className="bi bi-plus-circle"></i></button>
-                
             </div>
-            <div className="row col-lg-4">
+            <div class="col-auto">
+                <input ref={descripRef} placeholder='Descripción' className="form-control" type="text"></input>
+            </div>    
+               
+            <div class="col-auto">
+                <input ref={impoRef} type="checkbox" className="form-check-input me-2" id="importancia" font-color="white"></input>Importante
+            </div>
+            <div class="col-auto">
+                <button onClick={agregarTarea} className="btn btn-info ms-1">Ingresar Post it  <i className="bi bi-plus-circle"></i></button>
+            </div>  
+            </form>    
+            <div>
+            <div className="row col-3">
                 {todos.map((todo) => (
                     <TodoItem todo={todo} key={todo.id}></TodoItem>
                 ))}
             </div>
+            </div>
+            
+            </div>
+            
 
           
         </Fragment>
